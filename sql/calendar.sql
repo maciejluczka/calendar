0db6bfeb276cc2b78f7f6849e809afaedd01732d
Maciej Łuczka <maciejluczka@MacBook-Pro-Maciej.local>
2025-06-18 18:36:52 +0200

    db changes

--- sql/calendar.sql
+++ sql/calendar.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 23, 2025 at 10:01 PM
+-- Generation Time: Jun 08, 2025 at 11:26 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -18,99 +18,77 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `calendar`
+-- Database: `calendar3`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `eventsdsadas`
+-- Table structure for table `events`
 --
 
-CREATE TABLE `eventsdsadas` (
-  `date` varchar(20) DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
+CREATE TABLE `events` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `name` varchar(255) NOT NULL,
   `description` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `events`
+--
+
+INSERT INTO `events` (`id`, `user_id`, `date`, `name`, `description`) VALUES
+(1, 1, '2025-06-20', 'Urodziny mamy', 'Przyjęcie u mamy w domu'),
+(2, 1, '2025-06-25', 'Spotkanie zespołu', 'Omówienie projektu w biurze'),
+(3, 1, '2025-07-05', 'Grill z przyjaciółmi', 'Grill w ogrodzie u Tomka'),
+(4, 2, '2025-07-15', 'Ślub kuzyna', 'Ceremonia w kościele i przyjęcie weselne'),
+(5, 2, '2025-07-22', 'Wyjście do kina', 'Nowy film Marvela z paczką znajomych'),
+(6, 2, '2025-08-01', 'Szkolenie online', 'Szkolenie z nowego systemu CRM');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `eventstest`
+-- Table structure for table `event_groups`
 --
 
-CREATE TABLE `eventstest` (
-  `date` varchar(20) DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL
+CREATE TABLE `event_groups` (
+  `event_id` int(11) NOT NULL,
+  `group_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `eventstest`
+-- Dumping data for table `event_groups`
 --
 
-INSERT INTO `eventstest` (`date`, `name`, `description`) VALUES
-('15.5.2025', 'testdsadasd', 'eventsdsadasdsaddadasdasdsa'),
-('5.6.2025', 'dsadasd', 'eventsfasddasd'),
-('1.5.2025', '', 'events'),
-('1.5.2025', '', 'events'),
-('1.5.2025', '', 'events'),
-('1.5.2025', '', 'eventsadsad'),
-('2.5.2025', '', 'eventsdsadasfaf');
+INSERT INTO `event_groups` (`event_id`, `group_id`) VALUES
+(1, 1),
+(2, 3),
+(3, 2),
+(4, 1),
+(5, 2),
+(6, 3);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `eventstest1`
+-- Table structure for table `groups`
 --
 
-CREATE TABLE `eventstest1` (
-  `date` varchar(20) DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL
+CREATE TABLE `groups` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `eventstest1`
+-- Dumping data for table `groups`
 --
 
-INSERT INTO `eventstest1` (`date`, `name`, `description`) VALUES
-('2.5.2025', 'naa', 'dsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsadsa'),
-('3.5.2025', 'dsa', 'dsadas'),
-('11.5.2025', '', 'dsadfasdfasdfsadd'),
-('9.5.2025', '', ''),
-('10.5.2025', 'dasda', 'dsadasd');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `eventstest123`
---
-
-CREATE TABLE `eventstest123` (
-  `date` varchar(20) DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `eventstest123`
---
-
-INSERT INTO `eventstest123` (`date`, `name`, `description`) VALUES
-('9.5.2025', 'Urodziny', 'dsafasfdf');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `eventstestdsad`
---
-
-CREATE TABLE `eventstestdsad` (
-  `date` varchar(20) DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+INSERT INTO `groups` (`id`, `name`) VALUES
+(3, 'Praca'),
+(2, 'Przyjaciele'),
+(1, 'Rodzina');
 
 -- --------------------------------------------------------
 
@@ -121,40 +99,122 @@ CREATE TABLE `eventstestdsad` (
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `login` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `eventsTable` varchar(255) NOT NULL
+  `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `login`, `password`, `eventsTable`) VALUES
-(14, 'test', '098f6bcd4621d373cade4e832627b4f6', 'eventstest'),
-(15, 'test1', 'cc03e747a6afbbcbf8be7668acfebee5', 'eventstest1'),
-(16, 'dsadas', '89defae676abd3e3a42b41df17c40096', 'eventsdsadas'),
-(17, 'testdsad', '5676e37338d668578a3686ab581abe0f', 'eventstestdsad'),
-(18, 'test123', 'c06db68e819be6ec3d26c6038d8e8d1f', 'eventstest123');
+INSERT INTO `users` (`id`, `login`, `password`) VALUES
+(1, 'Tomek', '098f6bcd4621d373cade4e832627b4f6'),
+(2, 'Maciek', 'cc03e747a6afbbcbf8be7668acfebee5');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_groups`
+--
+
+CREATE TABLE `user_groups` (
+  `user_id` int(11) NOT NULL,
+  `group_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_groups`
+--
+
+INSERT INTO `user_groups` (`user_id`, `group_id`) VALUES
+(1, 1),
+(1, 3),
+(2, 2),
+(2, 3);
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `events`
+--
+ALTER TABLE `events`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `event_groups`
+--
+ALTER TABLE `event_groups`
+  ADD PRIMARY KEY (`event_id`,`group_id`),
+  ADD KEY `group_id` (`group_id`);
+
+--
+-- Indexes for table `groups`
+--
+ALTER TABLE `groups`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `login` (`login`);
+
+--
+-- Indexes for table `user_groups`
+--
+ALTER TABLE `user_groups`
+  ADD PRIMARY KEY (`user_id`,`group_id`),
+  ADD KEY `group_id` (`group_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `events`
+--
+ALTER TABLE `events`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `groups`
+--
+ALTER TABLE `groups`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `events`
+--
+ALTER TABLE `events`
+  ADD CONSTRAINT `events_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `event_groups`
+--
+ALTER TABLE `event_groups`
+  ADD CONSTRAINT `event_groups_ibfk_1` FOREIGN KEY (`event_id`) REFERENCES `events` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `event_groups_ibfk_2` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `user_groups`
+--
+ALTER TABLE `user_groups`
+  ADD CONSTRAINT `user_groups_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `user_groups_ibfk_2` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
